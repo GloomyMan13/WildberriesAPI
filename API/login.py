605563115b6708c0d711cd23231f7f25d577dfe1
GloomyMan13 <darthvaider13@gmail.com>
2021-09-20 17:05:18 +1000

    Rewrite imports

--- API/login.py
+++
@@ -1,8 +0,0 @@
-from requests.structures import CaseInsensitiveDict
-
-headers = CaseInsensitiveDict()
-headers["accept"] = "application/json"
-headers["Authorization"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." \
-                           "eyJhY2Nlc3NJRCI6ImJkZDdkODBmLTM5ZWMtN" \
-                           "DZjNS1hYzQwLWViZmU5ZWViMTEzMCJ9.xw0Nx" \
-                           "D2s58Uu-S8XrvKux6gvULzxRi_8PSiO9jdtQrA"
