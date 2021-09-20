7ee6ac518de150f4264c0bc91e2afb112060982d
GloomyMan13 <darthvaider13@gmail.com>
2021-09-20 15:02:18 +1000

    Rebuild architecture of class Getters

--- logic.py
+++ logic.py
@@ -10,24 +10,148 @@ class Getters:
 
     :method response: Return result of request.get in JSON
     """
-    def __init__(self, request_link, params, head=headers):
+    def __init__(self, key, head=headers, **kwargs):
         """
-        :param request_link: Link for request, str
-        :param params: parameters of get request, str
+        :param key: Link for request, str
         :param head: headers of get requests, CaseInsensitiveDict
+        :param params: parameters of get request, str
         """
-        if isinstance(request_link, str):
-            self.link = request_link
-        else:
-            raise ValueError('Link must be a string')
+
+        try:
+            key = key.lower()
+            self.link = Getters.__KEYS[key]
+        except KeyError:
+            print("Wrong keyword")
+        except AttributeError:
+            print('Key must be a string')
         if isinstance(headers, requests.structures.CaseInsensitiveDict):
             self.headers = head
         else:
             raise ValueError('Headers must be a dict')
-        if isinstance(params, str) or params is None:
-            self.params = params
+        self.params = self.__setparam(key, **kwargs)
+
+    @staticmethod
+    def __setparam(key, **kwargs):
+        if key == 'orders':
+            return Getters.__orders(**kwargs)
+        elif key == 'stocks':
+            return Getters.__stocks(**kwargs)
+        elif key == 'costs':
+            return Getters.__cost(**kwargs)
+
+    @staticmethod
+    def __stocks(search=None, skip=0, take=10, sort=None, order=None):
+        """
+        Create parameters string for get_stocks
+
+        :param search: None or str
+               Word or word fragment to search
+        :param skip: int
+               How much strings to skip (pagination)
+        :param take: int
+               How much strings to take (pagination)
+        :param sort: None or name of field to sort str(subject,
+                     brand, name, size, barcode, articles)
+               On which field sort
+        :param order: None or str(asc) or str(desc)
+               Order of sort: asc or desc
+        :return: str
+                parameters for get_stocks
+        """
+        param = '?'
+        RIGHT_SORT_LIST = ['subject', 'brand', 'name', 'size',
+                           'barcode', 'articles']
+        ORDER_LIST = ['asc', 'desc']
+        if isinstance(search, str):
+            param += ''.join(f'search={search}&')
+        elif not isinstance(search, str) and search is not None:
+            raise ValueError("search must be a str")
+        if not isinstance(skip, int):
+            raise ValueError('skip must be an int')
+        if not isinstance(take, int):
+            raise ValueError('take must be an int')
+        param += ''.join(f'skip={skip}&take={take}')
+        if sort in RIGHT_SORT_LIST:
+            param += ''.join(f'&sort={sort}')
+        elif sort is not None and sort not in RIGHT_SORT_LIST:
+            raise ValueError("sort must be subject, brand, name,"
+                             "size, barcode or articles")
+        if order in ORDER_LIST:
+            param += ''.join(f'&order={order}')
+        elif order is not None and order not in ORDER_LIST:
+            raise ValueError("order must be asc or desc")
+        return param
+
+    @staticmethod
+    def __orders(date_start=(datetime.today() - timedelta(days=1)), date_end=None, status=None, take=10, skip=0,
+                 order_id=None):
+        """
+        Create parameter string for get_order
+
+        :param date_start:  None or datetime obj
+               Date of starting of search, if None - yesterday
+        :param date_end: None or datetime obj
+               Date of ending of search, if None - now
+        :param status: None or int from 0 to 8 without 4
+               If None - all
+               0 = new order; 1 = accepted order; 2 = End of assembly task
+               3 = Declined assembly task; 5 = On delivery; 6 = Client taken delivery;
+               7 = Client refused to take delivery
+        :param take: None or int
+               If None = 10
+               Num of taken orders (pagination)
+        :param skip: int
+               If None = 0
+               Num of skipped orders (pagination)
+        :param order_id: None or int
+               Number of order
+        :return: str
+                 string of parameters
+        """
+        param = '?'
+        STATUSES_LIST = [0, 1, 2, 3, 5, 6, 7]
+        date_format = '%Y-%m-%dT'  # formatting time into RFC3339
+        if not isinstance(date_start, datetime):
+            raise ValueError("date_start must be datetime obj")
+        date_start = date_start.strftime(date_format)
+        param += ''.join(f'date_start={date_start}' +
+                         '00%3A00%3A00.000%2B10%3A00')
+        if isinstance(date_end, datetime):
+            date_end = date_end.strftime(date_format)
+            param += ''.join(f'&date_end={date_end}' +
+                             '00%3A00%3A00.000%2B10%3A00')
+        elif date_end is not None and not isinstance(date_end, datetime):
+            raise ValueError('date_end must be datetime obj')
+        if status in STATUSES_LIST:
+            param += ''.join(f'&status={status}')
+        elif status is not None and status not in STATUSES_LIST:
+            raise ValueError('status must be int in range from 0 to 7')
+        if not isinstance(take, int):
+            raise ValueError('take must be an int')
+        if not isinstance(skip, int):
+            raise ValueError('skip must be an int')
+        param += ''.join(f'&skip={skip}&take={take}')
+        if isinstance(order_id, int):
+            param += ''.join(f'&id={order_id}')
+        elif order_id is not None and not isinstance(order_id, int):
+            raise ValueError('order_id must be an int')
+        return param
+
+    @staticmethod
+    def __cost(quantity=1):
+        """
+        Create parameters string to get_cost obj
+
+        :param quantity:  0 - all, 1 - not null quantity, 2 - null quantity
+        :return: parameter string
+        """
+        param = ''
+        QUANTITY_LIST = [1, 2, 0]
+        if quantity not in QUANTITY_LIST:
+            raise ValueError('Quantity must be 1, 2 or 0')
         else:
-            raise ValueError('Params must be a string')
+            param.join(f'quantity={quantity}')
+        return param
 
     def response(self):
         """
@@ -39,130 +163,13 @@ class Getters:
                                 headers=self.headers)
         return response.json()
 
-
-def get_cost_param(quantity=1):
-    """
-    Create parameters string to get_cost obj
-
-    :param quantity:  0 - all, 1 - not null quantity, 2 - null quantity
-    :return: parameter string
-    """
-    param = ''
-    QUANTITY_LIST = [1, 2, 0]
-    if quantity not in QUANTITY_LIST:
-        raise ValueError('Quantity must be 1, 2 or 0')
-    else:
-        param.join(f'quantity={quantity}')
-    return param
+    __KEYS = {
+        'orders': 'https://suppliers-api.wildberries.ru/api/v2/orders',
+        'warehouses': "https://suppliers-api.wildberries.ru/api/v2/warehouses",
+        'costs': "https://suppliers-api.wildberries.ru/public/api/v1/info",
+        'stocks': "https://suppliers-api.wildberries.ru/api/v2/stocks"
+    }
 
 
-url_cost = f"https://suppliers-api.wildberries.ru/public/api/v1/info"
 
 
-def get_market_param(search=None, skip=0, take=10, sort=None,
-                     order=None):
-    """
-    Create parameters string for get_stocks
-
-    :param search: None or str
-           Word or word fragment to search
-    :param skip: int
-           How much strings to skip (pagination)
-    :param take: int
-           How much strings to take (pagination)
-    :param sort: None or name of field to sort str(subject,
-                 brand, name, size, barcode, articles)
-           On which field sort
-    :param order: None or str(asc) or str(desc)
-           Order of sort: asc or desc
-    :return: str
-            parameters for get_stocks
-    """
-    param = '?'
-    RIGHT_SORT_LIST = ['subject', 'brand', 'name', 'size',
-                       'barcode', 'articles']
-    ORDER_LIST = ['asc', 'desc']
-    if isinstance(search, str):
-        param += ''.join(f'search={search}&')
-    elif not isinstance(search, str) and search is not None:
-        raise ValueError("search must be a str")
-    if not isinstance(skip, int):
-        raise ValueError('skip must be an int')
-    if not isinstance(take, int):
-        raise ValueError('take must be an int')
-    param += ''.join(f'skip={skip}&take={take}')
-    if sort in RIGHT_SORT_LIST:
-        param += ''.join(f'&sort={sort}')
-    elif sort is not None and sort not in RIGHT_SORT_LIST:
-        raise ValueError("sort must be subject, brand, name,"
-                         "size, barcode or articles")
-    if order in ORDER_LIST:
-        param += ''.join(f'&order={order}')
-    elif order is not None and order not in ORDER_LIST:
-        raise ValueError("order must be asc or desc")
-    return param
-
-
-marketplace_url = "https://suppliers-api.wildberries.ru/api/v2/stocks"
-
-warehouse_url = "https://suppliers-api.wildberries.ru/api/v2/warehouses"
-
-
-def get_orders_param(date_start=(datetime.today() - timedelta(days=1)),
-                     date_end=None, status=None, take=10, skip=0,
-                     order_id=None):
-    """
-    Create parameter string for get_order
-
-    :param date_start:  None or datetime obj
-           Date of starting of search, if None - yesterday
-    :param date_end: None or datetime obj
-           Date of ending of search, if None - now
-    :param status: None or int from 0 to 8 without 4
-           If None - all
-           0 = new order; 1 = accepted order; 2 = End of assembly task
-           3 = Declined assembly task; 5 = On delivery; 6 = Client taken delivery;
-           7 = Client refused to take delivery
-    :param take: None or int
-           If None = 10
-           Num of taken orders (pagination)
-    :param skip: int
-           If None = 0
-           Num of skipped orders (pagination)
-    :param order_id: None or int
-           Number of order
-    :return: str
-             string of parameters
-    """
-    param ='?'
-    STATUSES_LIST = [0, 1, 2, 3, 5, 6, 7]
-    date_format = '%Y-%m-%dT'  # formatting time into RFC3339
-    if not isinstance(date_start, datetime):
-        raise ValueError("date_start must be datetime obj")
-    date_start = date_start.strftime(date_format)
-    param += ''.join(f'date_start={date_start}' +
-                     '00%3A00%3A00.000%2B10%3A00')
-    if isinstance(date_end, datetime):
-        date_end = date_end.strftime(date_format)
-        param += ''.join(f'&date_end={date_end}' +
-                         '00%3A00%3A00.000%2B10%3A00')
-    elif date_end is not None and not isinstance(date_end, datetime):
-        raise ValueError('date_end must be datetime obj')
-    if status in STATUSES_LIST:
-        param += ''.join(f'&status={status}')
-    elif status is not None and status not in STATUSES_LIST:
-        raise ValueError('status must be int in range from 0 to 7')
-    if not isinstance(take, int):
-        raise ValueError('take must be an int')
-    if not isinstance(skip, int):
-        raise ValueError('skip must be an int')
-    param += ''.join(f'&skip={skip}&take={take}')
-    if isinstance(order_id, int):
-        param += ''.join(f'&id={order_id}')
-    elif order_id is not None and not isinstance(order_id, int):
-        raise ValueError('order_id must be an int')
-    print(param)
-    return param
-
-
-orders_url = 'https://suppliers-api.wildberries.ru/api/v2/orders'
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from logic import Getters, url_cost, get_cost_param, marketplace_url,\
-    get_market_param, warehouse_url, orders_url, get_orders_param
+from logic import Getters
+from login import headers
 
-get_costs = Getters(url_cost, get_cost_param())
-get_stocks = Getters(marketplace_url, get_market_param())
-warehouse = Getters(warehouse_url, None)
-get_orders = Getters(orders_url, get_orders_param())
+orders = 'stocks'
+get_orders = Getters(orders, head=headers, take=20)
 print(get_orders.response())
+
